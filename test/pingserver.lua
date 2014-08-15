aa250df90781bab71f0e0438a7063bec1c0b7a23
ghost <gaoshi_chen@foxmail.com>
2014-08-15 16:04:10 +0800

    添加在线更新数据
    添加在线更新数据

--- test/pingserver.lua
+++ test/pingserver.lua
@@ -3,6 +3,43 @@ local queue = require "skynet.queue"
 
 local i = 0
 local hello = "hello"
+test1 = {1,2}
+test2 = nil
+if loadfile then
+	_ModTbl = {}
+	local mainfunc = nil
+	local path = skynet.getenv "snax"
+	local errlist = {}
+	for pat in string.gmatch(path,"[^;]+") do
+		local filename = string.gsub(pat, "?", "testtbl")
+		local f , err = loadfile(filename, "bt", _ModTbl)
+		if f then
+			mainfunc = f
+			break
+		else
+			table.insert(errlist, err)
+		end
+	end
+	if mainfunc then
+		mainfunc()
+	else
+		error(errlist)
+	end
+	mainfunc()
+	testtbl = _ModTbl.gettt()
+--	print("_Mod, _ModTbl, testtbl, testtbl[1]:", _Mod, _ModTbl, testtbl, testtbl[1])
+end
+
+--tbl = {}
+--
+--function testfunc1()
+--end
+--
+--function tbl.testfunc2()
+--end
+--
+--local function testfunc3()
+--end
 
 function response.ping(hello)
 	skynet.sleep(100)
@@ -28,7 +65,7 @@ end
 function accept.hello()
 	lock(function()
 	i = i + 1
-	print (i, hello)
+	print (i, hello, test1[1], test1[2], test2, testtbl[1], testtbl[2])
 	end)
 end
 
